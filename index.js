0a2e68b6ce983b4a1337b5e8d89dfcbd83e208a2
Bo Brockmann <bbrockmann1@gmail.com>
2022-09-19 16:21:34 -0600

    commit tree d9a8616722ca04dc7e37cb337f9f1ee807f1d154

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+};
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+};
+
+function wrapAdjective(adjective = '*'){
+    switch (true){
+        case emphatic ===  '*':
+            `You are ${adjective}a hard worker${adjective}`
+}}
+
+
+function wrapAdjective(emphatic){
+    return function(adjective){
+        return `You are ${emphatic}${adjective}${emphatic}!`
+    }
+};
